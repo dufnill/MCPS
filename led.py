2c2e14fbbc5aea6a8333e0ccbb262076355cff30
dufnill <davide.fiorini.davide@gmail.com>
2022-05-06 14:45:58 +0200

    Variables removed from led

--- led.py
+++ led.py
@@ -4,18 +4,6 @@ import RPi.GPIO as gp
 from service_funct import subscribe_to_topic
 from MQTTClient import MQTTClient
 
-BLUE_PIN = 15
-RED_PIN  = 16
-
-gp.setwarnings(False)
-gp.setmode(gp.BOARD)
-
-gp.setup(RED_PIN, gp.OUT)
-gp.setup(BLUE_PIN, gp.OUT)
-
-gp.output(RED_PIN, False)
-gp.output(BLUE_PIN, False)
-
 client  = MQTTClient("LED")
 
 user = "led"
@@ -23,4 +11,4 @@ psw  = "password"
 conn = "connection_string"
 topl = "/plant/led"
 
-subscribe_to_topic(client, user, psw, conn, topic, 1)
+subscribe_to_topic(client, user, psw, conn, topl, 1)
