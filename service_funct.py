b3af50df0004f3ef01473a75bb08be2a66080620
Davide Fiorini <91680810+dufnill@users.noreply.github.com>
2022-05-07 18:19:02 +0200

    Delete service_funct.py

--- service_funct.py
+++
@@ -1,8 +0,0 @@
-from MQTTClient import MQTTClient
-
-
-def subscribe_to_topic(sub: MQTTClient, usr: str, psw: str, con_str: str, port: int, topic: str, qos: int):
-    
-    sub.connect(usr, psw, con_str, port)
-    sub.subscribe(topic, qos)
-    
